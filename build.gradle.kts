9528bd8dcbe40da4196a63a045df45f969bd0583
yangwtang1210 <1757239496@qq.com>
2026-07-15 14:25:46 +0800

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,39 +1,10 @@
-plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-}
-
-android {
-    namespace = "com.remote.kvm"
-    compileSdk = 35
-
-    defaultConfig {
-        applicationId = "com.remote.kvm"
-        minSdk = 26
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0-phase1"
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
     }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"))
-        }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.7.3")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.21")
     }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-}
-
-dependencies {
-    implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
 }
